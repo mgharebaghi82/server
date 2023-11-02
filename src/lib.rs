153934c98e9ff11203c12a8bef011c7401ed95e0
mohammad <mgharebaghi82@gmail.com>
2023-11-02 19:34:00 +0330

    change openssl

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use axum::Router;
-use axum_server::tls_openssl::OpenSSLConfig;
+use axum_server::tls_rustls::RustlsConfig;
 use routers::create_routes;
 use std::net::SocketAddr;
 
@@ -8,16 +8,16 @@ mod routers;
 pub async fn run() {
     let app: Router = create_routes();
 
-    let config = OpenSSLConfig::from_pem_file(
+    let config = RustlsConfig::from_pem_file(
         "/etc/letsencrypt/live/centichain.org/fullchain.pem",
         "/etc/letsencrypt/live/centichain.org/privkey.pem",
     )
-    .unwrap();
+    .await.unwrap();
 
     let addr = SocketAddr::from(([0,0,0,0], 3002));
     println!("listening on: {}", addr);
 
-    axum_server::bind_openssl(addr, config)
+    axum_server::bind_rustls(addr, config)
         .serve(app.into_make_service())
         .await
         .unwrap();
