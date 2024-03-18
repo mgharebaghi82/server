c069599888151f00d21fa23e4a5c0afacdc8d772
mohammad <mgharebaghi82@gmail.com>
2024-03-18 20:55:25 +0330

    change keep alive of sse

--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -333,38 +333,42 @@ async fn utxo_sse() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     let blockchain_coll: Collection<Document> = blockchain_db().await.collection("Blocks");
     let mut wathcing = blockchain_coll.watch(None, None).await.unwrap();
 
-    match wathcing.next().await {
-        Some(_change) => match _change {
-            Ok(_ch) => {
-                let mut cursor = blockchain_coll.find(None, None).await.unwrap();
-                let mut generated_centis = Decimal::from_str("0.0").unwrap();
-                let all_centies = Decimal::from_str("21000000.0").unwrap();
-                while let Some(doc) = cursor.next().await {
-                    match doc {
-                        Ok(document) => {
-                            let block: Block = from_document(document).unwrap();
-                            generated_centis +=
-                                block.body.coinbase.coinbase_data.reward.round_dp(12);
+    async move { 
+        loop {
+            match wathcing.next().await {
+                Some(_change) => match _change {
+                    Ok(_ch) => {
+                        let mut cursor = blockchain_coll.find(None, None).await.unwrap();
+                        let mut generated_centis = Decimal::from_str("0.0").unwrap();
+                        let all_centies = Decimal::from_str("21000000.0").unwrap();
+                        while let Some(doc) = cursor.next().await {
+                            match doc {
+                                Ok(document) => {
+                                    let block: Block = from_document(document).unwrap();
+                                    generated_centis +=
+                                        block.body.coinbase.coinbase_data.reward.round_dp(12);
+                                }
+                                Err(_) => break,
+                            }
                         }
-                        Err(_) => break,
-                    }
-                }
-                let centies = (all_centies - generated_centis.round_dp(12)).to_string();
-                match tx.send(Ok(Event::default().data(centies))) {
-                    Ok(_) => {
-                        println!("event sent");
+                        let centies = (all_centies - generated_centis.round_dp(12)).to_string();
+                        match tx.send(Ok(Event::default().data(centies))) {
+                            Ok(_) => {
+                                println!("event sent");
+                            }
+                            Err(_e) => {
+                                // println!("error line 359: {_e}")
+                            }
+                        }
                     }
                     Err(_e) => {
-                        // println!("error line 359: {_e}")
+                        // println!("error line 364: {_e}")
                     }
-                }
+                },
+                None => {}
             }
-            Err(_e) => {
-                // println!("error line 364: {_e}")
-            }
-        },
-        None => {}
-    }
+        }
+    }.await;
 
     Sse::new(stream)
 }
