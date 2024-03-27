3d77ab7fba95505412fec27c72153fc67bfdfba4
mohammad <mgharebaghi82@gmail.com>
2024-03-27 16:19:02 +0330

    sleep for ws loop

--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -306,7 +306,7 @@ struct Pagination {
 async fn handle_blockchain(extract::Json(data): extract::Json<Pagination>) -> Json<Vec<Block>> {
     let mut blocks = Vec::new();
     let blocks_coll: Collection<Document> = blockchain_db().await.collection("Blocks");
-    let filter = doc! {"header.number": {"$get": 1, "$lte": 5}};
+    let filter = doc! {"header.number": {"$gte": 1, "$lte": 5}};
     let mut cursor = blocks_coll.find(filter, None).await.unwrap();
     while let Some(doc) = cursor.next().await {
         match doc {
